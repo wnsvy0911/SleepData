c64ee0099e9573e2b614765070feb55fec66f251
Junpyo Kim <jpkimkorea@gmail.com>
2022-02-04 14:58:43 -0600

    Create user options - Enter 1 to create data file - Enter 2 to parse data file

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace SleepData
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+
+            if (resp == "1")
+            {
+                // TODO: create data file
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+
+            }
         }
     }
 }
